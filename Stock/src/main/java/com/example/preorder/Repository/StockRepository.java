19f6ef4f94cffdef8c841daf19004351684b8c0e
Sunmyeong Jin <tjsaud3250@naver.com>
2024-03-03 21:27:02 +0900

    Redis Repository 적용

--- Stock/src/main/java/com/example/preorder/Repository/StockRepository.java
+++ Stock/src/main/java/com/example/preorder/Repository/StockRepository.java
@@ -2,9 +2,10 @@ package com.example.preorder.Repository;
 
 import com.example.preorder.Entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface StockRepository extends JpaRepository<Stock,Long> {
+public interface StockRepository extends CrudRepository<Stock,Long> {
     Optional<Stock> findByProductId(Long productId);
 }
